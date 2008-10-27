ef2c7acbef8209867c47d99a1c6abaae76f4e42a
Leonid Evdokimov <leon@darkk.net.ru>
2008-10-27 20:26:36 +0600

    New upstream release - v2.64 (2008-10-21)
     * Gracefully handle missing charsets
     * Friendlier and more useful message if sendmail isn't installed
     * SunOS locking fix

--- rss2email.py
+++ rss2email.py
@@ -10,7 +10,7 @@ Usage:
   list
   delete n
 """
-__version__ = "2.63"
+__version__ = "2.64"
 __author__ = "Aaron Swartz (me@aaronsw.com)"
 __copyright__ = "(C) 2004 Aaron Swartz. GNU GPL 2 or 3."
 ___contributors__ = ["Dean Jackson", "Brian Lalor", "Joey Hess", 
@@ -117,7 +117,7 @@ def send(sender, recipient, subject, body, contenttype, extraheaders=None, smtps
 	for body_charset in CHARSET_LIST:
 	    try:
 	        body.encode(body_charset)
-	    except UnicodeError:
+	    except (UnicodeError, LookupError):
 	        pass
 	    else:
 	        break
@@ -185,10 +185,23 @@ def send(sender, recipient, subject, body, contenttype, extraheaders=None, smtps
 		return smtpserver
 
 	else:
-		i, o = os.popen2(["/usr/sbin/sendmail", recipient])
-		i.write(msg_as_string)
-		i.close(); o.close()
-		del i, o
+		try:
+			i, o = os.popen2(["/usr/sbin/sendmail", recipient])
+			i.write(msg_as_string)
+			i.close(); o.close()
+			del i, o
+		except:
+			print '''Error attempting to send email via sendmail. Possibly you need to configure your config.py to use a SMTP server? Please refer to the rss2email documentation or website (http://rss2email.infogami.com) for complete documentation of config.py. The options below may suffice for configuring email:
+# 1: Use SMTP_SERVER to send mail.
+# 0: Call /usr/sbin/sendmail to send mail.
+SMTP_SEND = 0
+
+SMTP_SERVER = "smtp.yourisp.net:25"
+AUTHREQUIRED = 0 # if you need to use SMTP AUTH set to 1
+SMTP_USER = 'username'  # for SMTP AUTH, set SMTP username here
+SMTP_PASS = 'password'  # for SMTP AUTH, set SMTP password here
+'''
+			sys.exit(1)
 		return None
 
 ## html2text options ##
@@ -412,11 +425,15 @@ def load(lock=1):
 		sys.exit(1)
 	feeds = pickle.load(feedfileObject)
 	if lock:
-		if unix: fcntl.flock(feedfileObject.fileno(), fcntl.LOCK_EX)
+		locktype = 0
+		if unix:
+			locktype = fcntl.LOCK_EX
+			if (sys.platform.find('sunos')): locktype = fcntl.LOCK_SH
+			fcntl.flock(feedfileObject.fileno(), locktype)
 		#HACK: to deal with lock caching
 		feedfileObject = open(feedfile, 'r')
 		feeds = pickle.load(feedfileObject)
-		if unix: fcntl.flock(feedfileObject.fileno(), fcntl.LOCK_EX)
+		if unix: fcntl.flock(feedfileObject.fileno(), locktype)
 
 	return feeds, feedfileObject
 
